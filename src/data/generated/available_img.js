491d6c5e3721e23c95c12dfb03c2cad3ab727963
Muyan925 <3047169175@qq.com>
2025-05-16 19:08:00 +0800

    Initial commit with SurVis project files

---
+++ src/data/generated/available_img.js
@@ -0,0 +1,13 @@
+const availableImg = ["Beck2016Visual",
+                      "brown2020language",
+                      "lee2018pre",
+                      "xu2024large",
+                      "nguyen2025towards",
+                      "karbab2019maldy",
+                      "ucci2019survey",
+                      "ferrag2024revolutionizing",
+                      "kheddar2024transformers",
+                      "shayegani2023survey",
+                      "motlagh2024large"
+
+];
